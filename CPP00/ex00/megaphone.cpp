d4091a49855401ca4d344e4ed2a927af802733c2
Artur Silva <artuda-s@c2r4s3.42porto.com>
2024-12-17 17:56:43 +0000

    CP00 ex1

---
+++ CPP00/ex00/megaphone.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+
+int main (int ac, char **av)
+{
+    if (ac == 1)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+        return 1;
+    }
+
+    for (int i = 1; i < ac; i++)
+    {
+        std::string str = av[i]; 
+        for (int j = 0; str[j]; j++)
+        {
+            if (islower(str[j]))
+                str[j] = toupper(str[j]);
+        }
+        std::cout << str;
+    }
+    std::cout << '\n';
+    return 0;
+}

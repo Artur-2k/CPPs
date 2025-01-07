37ab1dd2f5008a6fa3fcb39faddb3b97896fc4f1
Artur Carvalho <arturcarvalho0507@gmail.com>
2025-01-07 21:55:07 +0000

    CPP01: ex05 fix messages

--- CPP01/ex05/src/main.cpp
+++ CPP01/ex05/src/main.cpp
@@ -4,10 +4,20 @@ int main()
 {
 	Harl satan;
 
+	std::cout << "[DEBUG]" << std::endl;
 	satan.complain("DEBUG");
+
+	std::cout << "\n[INFO]" << std::endl;
 	satan.complain("INFO");
+
+	std::cout << "\n[WARNING]" << std::endl;
 	satan.complain("WARNING");
+
+	std::cout << "\n[ERROR]" << std::endl;
 	satan.complain("ERROR");
+
+	std::cout << "\n[UNKNOWN]" << std::endl;
 	satan.complain("unknown");
+
 	return 0;
 }

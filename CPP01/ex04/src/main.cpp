56f30ed275cf0ea33bff1aff9af36119f072c3eb
Artur Carvalho <arturcarvalho0507@gmail.com>
2025-01-07 17:14:48 +0000

    CPP01: ex04

--- CPP01/ex04/src/main.cpp
+++ CPP01/ex04/src/main.cpp
@@ -1,71 +1,11 @@
-#include <iostream>
-#include <string>
-#include <fstream>
-
-
-using std::cout;
-using std::string;
-using std::endl;
+#include "sed.hpp"
 
 int main(int ac, char *av[])
 {
-    if (ac != 4)
-    {
-        cout << "./Sed_is_for_losers filename string1 string2" << endl;
-        exit(1);
-    }
+	(ac != 4) ? exit(1) : void();
 
-    string filename = av[1], s1 = av[2], s2 = av[3];
+	sed(av);
 
-    std::ifstream inFile(filename.c_str());
-    if (!inFile)
-    {
-        cout << "Error opening file" << endl;
-        exit (1);
-    }
-
-    string content;
-    {
-        string line;
-        bool firstLine = true;
-
-        while (true)
-        {
-            std::getline(inFile, line);
-            if (inFile.eof())
-                break ;
-            if (!firstLine)
-                content += "\n";
-            firstLine = false;
-            content += line;
-        }
-    }
-    inFile.close(); 
-    if (content.empty())
-        exit(0);
-
-    // std::string::npos is kinda like an error return to some functions
-    // look for s1 in content, erase it and insert at that position s2 (same as replace xd) and update the index
-    {
-        size_t i = 0;
-        while((i = content.find(s1, i)) != std::string::npos)
-        {
-            content.erase(i, s1.length());
-            content.insert(i, s2);
-            i+= s2.length();
-        }
-    }
-
-    std::ofstream outFile(filename + ".replace");
-    if (!outFile)
-    {
-        cout << "Error opening the output file" << endl;
-        exit (1);
-    }
-
-    outFile << content;
-    outFile.close();
-
-    cout << "GeT SeDed XDDD" << endl;   
+    std::cout << "GeT SeDed XDDD" << std::endl;
     return 0;
 }
---
+++ CPP01/ex04/src/sed.cpp
@@ -0,0 +1,65 @@
+#include "sed.hpp"
+
+static std::string &read_file_to_buffer(std::string &filename, std::string &buffer)
+{
+	// Opening file
+	std::ifstream inFile(filename.c_str());
+	if (!inFile)
+    {
+        std::cerr << "Error opening file" << std::endl;
+        exit (1);
+    }
+
+	// Sack memory for correct content copy if s1 spans across two lines
+    {
+        std::string line;
+	    while (std::getline(inFile, line))
+		{
+	        buffer += line;
+
+	        if (!inFile.eof())
+	            buffer += "\n";
+	    }
+    }
+    inFile.close();
+	// Nothing to sed
+    if (buffer.empty()) exit(0);
+	else return buffer;
+}
+
+// std::string::npos is kinda like an error return to some functions
+// look for s1 in content, erase it and insert at that position s2 (same as replace xd) and update the index
+static std::string &find_and_replace(std::string &buffer, std::string &find, std::string &replace)
+{
+    size_t i = 0;
+    while((i = buffer.find(find, i)) != std::string::npos)
+    {
+        buffer.erase(i, find.length());
+        buffer.insert(i, replace);
+        i+= replace.length();
+    }
+	return buffer;
+}
+
+static void write_to_file(const char *newFilename, std::string &buffer)
+{
+   std::ofstream outFile(newFilename);
+    if (!outFile)
+    {
+        std::cerr << "Error opening file" << std::endl;
+        exit (1);
+    }
+	// Write to the new file
+    outFile << buffer;
+    outFile.close();
+	return ;
+}
+
+void sed(char **av)
+{
+    std::string filename = av[1], s1 = av[2], s2 = av[3], content;
+
+	read_file_to_buffer(filename, content);
+	find_and_replace(filename, s1, s2);
+	write_to_file((filename + ".replace").c_str(), content);
+}

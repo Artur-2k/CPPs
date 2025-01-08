88fe5f537a1011edf3cc46cd87534a418c29820d
Artur Silva <arturcarvalho0507@gmail.com>
2025-01-08 19:43:35 +0000

    CPP01: ex01

---
+++ CPP02/ex01/Fixed.cpp
@@ -0,0 +1,66 @@
+#include "Fixed.hpp"
+
+const int Fixed::bits = 8;
+
+Fixed::Fixed() : value(0)
+{
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &other)
+{
+    std::cout << "Copy constructor called" << std::endl;
+    *this = other;
+}
+
+Fixed::Fixed(const int num) : value(num * (1 << bits))
+{
+    std::cout << "Int constructor called" << std::endl;
+}
+
+Fixed::Fixed(const float num) : value(static_cast<int>(roundf(num * (1  << bits))))
+{
+    std::cout << "Float constructor called" << std::endl;
+}
+
+
+Fixed::~Fixed()
+{
+    std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::toInt ( void ) const
+{
+    return (value >> bits);
+}
+
+float Fixed::toFloat ( void ) const
+{
+    return (static_cast<float>(value) / (1 << bits));
+}
+
+int Fixed::getRawBits( void ) const
+{
+    std::cout << "getRawBits member function called" << std::endl;
+    return value;
+}
+
+void Fixed::setRawBits( int const raw )
+{
+    std::cout << "setRawBits member function called" << std::endl;
+    value = raw;
+}
+
+Fixed &Fixed::operator = (const Fixed &other)
+{
+    std::cout << "Copy assignment operator called" << std::endl;
+    if (this != &other)
+        this->value = other.getRawBits();
+    return *this;
+}
+
+std::ostream &operator <<(std::ostream &os, Fixed const &fixedPoint)
+{
+    os << fixedPoint.toFloat();
+    return os;
+}
---
+++ CPP02/ex01/Fixed.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <iostream>
+#include <cmath>
+
+class Fixed
+{
+    private:
+        int value;
+        static const int bits;
+    public:
+        Fixed();
+        Fixed(const Fixed &other);
+        Fixed(const int num);
+        Fixed(const float num);
+        ~Fixed();
+
+
+        int toInt ( void ) const;
+        float toFloat ( void ) const;
+
+        int getRawBits( void ) const;
+        void setRawBits( int const raw );
+        Fixed &operator = (const Fixed &other);
+};
+
+std::ostream &operator <<(std::ostream &so, const Fixed &fixedPoint);

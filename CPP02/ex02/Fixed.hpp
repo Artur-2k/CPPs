e71661d0ebc234ca7cb56d1f7eb1ac9c8a00b604
Artur Silva <arturcarvalho0507@gmail.com>
2025-01-09 14:47:39 +0000

    CPP02: ex02

---
+++ CPP02/ex02/Fixed.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <iostream>
+#include <cmath>
+
+class Fixed
+{
+    private:
+        int value;
+        static const int bits;
+    public:
+        Fixed();
+        Fixed(const Fixed &other);
+        Fixed(const int num);
+        Fixed(const float num);
+        ~Fixed();
+
+
+        int toInt ( void ) const;
+        float toFloat ( void ) const;
+
+        int getRawBits( void ) const;
+        void setRawBits( int const raw );
+        Fixed &operator = (const Fixed &other);
+        Fixed &operator + (const Fixed &other){
+            Fixed ret (*this);
+            ret.value += other.getRawBits();
+            return ret;
+        }
+};
+
+std::ostream &operator <<(std::ostream &so, const Fixed &fixedPoint);

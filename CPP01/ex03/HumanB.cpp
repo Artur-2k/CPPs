5a111b00e0264c5259df60760043fb1725b00782
Artur Carvalho <arturcarvalho0507@gmail.com>
2025-01-28 22:16:04 +0000

    UPDATED MAKEFILE and added 42 Header

--- CPP01/ex03/HumanB.cpp
+++ CPP01/ex03/HumanB.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   HumanB.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: artuda-s <artuda-s@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/28 22:00:59 by artuda-s          #+#    #+#             */
+/*   Updated: 2025/01/28 22:01:02 by artuda-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "HumanB.hpp"
 
 HumanB::HumanB(std::string name) : _name(name) ,_wpn(NULL) {};
@@ -8,7 +20,6 @@ void HumanB::setWeapon(Weapon &wpn)
 {
     this->_wpn = &wpn;
 }
-
 
 void HumanB::attack() const
 {

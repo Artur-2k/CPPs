96e9a7d286a3991b9fafba8376a775dd8df5f926
Artur Silva <arturcarvalho0507@gmail.com>
2025-01-28 18:02:49 +0000

    CPP01::Makefiles and variable name on ex02

--- CPP01/ex02/main.cpp
+++ CPP01/ex02/main.cpp
@@ -6,16 +6,16 @@ using std::cout;
 int main()
 {
     std::string str = "HI THIS IS BRAIN";
-    std::string *ptrStr = &str;
-    std::string &adrStr = str;
+    std::string *stringPTR = &str;
+    std::string &stringREF = str;
 
     cout << "The memory address of the string variable is: " << &str << std::endl;
-    std::cout << "The memory address held by stringPTR " << ptrStr<< std::endl;
-    std::cout << "The memory address held by stringREF: " << &adrStr<< std::endl;
+    std::cout << "The memory address held by stringPTR " << stringPTR<< std::endl;
+    std::cout << "The memory address held by stringREF: " << &stringREF << std::endl;
 
     std::cout << "The value of the string variable: " << str << std::endl;
-    std::cout << "The value pointed to by stringPTR: " << *ptrStr << std::endl;
-    std::cout << "The value pointed to by stringREF: " << adrStr << std::endl;
+    std::cout << "The value pointed to by stringPTR: " << *stringPTR << std::endl;
+    std::cout << "The value pointed to by stringREF: " << stringREF << std::endl;
 
     return 0;
 }

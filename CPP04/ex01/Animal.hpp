7796fae6c073f60211c575feee58d425ffcc6ea2
Artur Silva <arturcarvalho0507@gmail.com>
2025-02-10 15:45:25 +0000

    CPP04:ex01 struct for the exercise

---
+++ CPP04/ex01/Animal.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: artuda-s <artuda-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/04 13:50:34 by artuda-s          #+#    #+#             */
+/*   Updated: 2025/02/10 14:40:57 by artuda-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <string>
+#include <iostream>
+
+class Animal
+{
+    protected:
+        std::string type;
+        
+    public:
+        Animal();
+        Animal(const Animal &other);
+        virtual ~Animal();
+
+        Animal& operator= (const Animal& other);
+
+        std::string getType() const;
+        virtual void makeSound() const;
+};
---
+++ CPP04/ex01/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: artuda-s <artuda-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/04 14:12:52 by artuda-s          #+#    #+#             */
+/*   Updated: 2025/02/10 15:44:55 by artuda-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Animal.hpp"
+#include "Dog.hpp"
+#include "Cat.hpp"
+
+#define RESET   "\033[0m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN    "\033[36m"
+#define WHITE   "\033[37m"
+
+int main()
+{
+    
+    return 0;
+}

85cf3fbb3faf3100bfebbf05c513038ac1cfc98d
fang <arturcarvalho0507@gmail.com>
2025-02-12 22:34:07 +0000

    CPP05:ex00 working but without the class cpp

--- CPP05/ex00/Bureaucrat.hpp
+++ CPP05/ex00/Bureaucrat.hpp
@@ -3,44 +3,75 @@
 /*                                                        :::      ::::::::   */
 /*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: artuda-s <artuda-s@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fang <fang@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 14:23:33 by artuda-s          #+#    #+#             */
-/*   Updated: 2025/02/12 18:04:14 by artuda-s         ###   ########.fr       */
+/*   Updated: 2025/02/12 22:31:17 by fang             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
 #include <string>
 #include <exception>
-
+#include <iostream>
 class Bureaucrat
 {
     private:
-        std::string _name;
+        const std::string _name;
         int _grade;
     public:
-    Bureaucrat() : _name("unknown"), _grade(0) {};
-    Bureaucrat(const Bureaucrat &other) { *this = other; };
-    ~Bureaucrat();
+    Bureaucrat() : _name("unknown"), _grade(150) {};
+    Bureaucrat(const std::string &name, int grade) : _name(name) 
+    {
+        if (grade < 1) throw GradeTooHighException();
+        
+        if (grade > 150) throw GradeTooLowException();
+        
+        _grade = grade;
+    }
+    Bureaucrat(const Bureaucrat &other) : _name(other._name), _grade(other._grade) {}
+    ~Bureaucrat() {}
     Bureaucrat operator=(const Bureaucrat &other) 
     {
         if (this != &other)
         {
-            this->_name = other._name;
             this->_grade = other._grade;
         }
         return *this;
     }
-        
-    class GradeTooHighException : std::exception
+
+    const std::string& getName() const 
+    {
+        return _name;
+    }
+     
+    int getGrade() const
+    {
+        return _grade;
+    } 
+
+    void incrementGrade()
+    {
+        if (_grade == 1) throw GradeTooHighException();
+        _grade--;
+    }
+    
+    void decrementGrade()
+    {
+        if (_grade == 150) throw GradeTooLowException();
+        _grade++;
+    }
+
+    class GradeTooHighException : public std::exception
     {
         public:
             virtual const char* what() const throw(){
                 return "Grade is Too High";
             }
     };
-    
-    class GradeTooLowException : std::exception
+
+    class GradeTooLowException : public std::exception
     {
         public:
             virtual const char* what() const throw(){
@@ -48,3 +79,9 @@ class Bureaucrat
             }
     };
 };
+
+std::ostream& operator<<(std::ostream&os, const Bureaucrat &bureaucrat)
+{
+    os << bureaucrat.getName() << ", bureaucrat grade " << bureaucrat.getGrade() << std::endl;
+    return os;
+}
---
+++ CPP05/ex00/main.cpp
@@ -0,0 +1,129 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fang <fang@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/12 22:15:27 by fang              #+#    #+#             */
+/*   Updated: 2025/02/12 22:30:31 by fang             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+#include "iostream"
+
+#define RESET   "\033[0m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN    "\033[36m"
+#define WHITE   "\033[37m"
+
+int main()
+{
+{  
+    std::cout << GREEN "Valid bureaucrat" RESET << std::endl;
+    try
+    {
+        Bureaucrat duck(std::string("Arturito"), 75);
+        std::cout << duck;
+
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+}
+    
+    std::cout << "//////////////////////////////////////////" << std::endl;
+{
+    std::cout << RED "Invalid too low bureaucrat" RESET << std::endl;
+    try
+    {
+        Bureaucrat duck(std::string("Manelito"), 200);
+        std::cout << duck;
+
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+}    
+    
+    std::cout << "//////////////////////////////////////////" << std::endl;
+{
+    std::cout << RED "Invalid too high bureaucrat" RESET << std::endl;
+    try
+    {
+        Bureaucrat duck(std::string("Manfarrico"), 0);
+        std::cout << duck;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+}    
+    
+    std::cout << "//////////////////////////////////////////" << std::endl;
+
+{
+    std::cout << GREEN "Valid increment and decrement grade" RESET << std::endl;
+    try
+    {
+        Bureaucrat duck(std::string("Tone"), 50);
+        std::cout << duck;
+        
+        std::cout << "incrementGrade:\n";
+        duck.incrementGrade();
+        std::cout << duck;
+        
+        std::cout << "decrementGrade:\n";
+        duck.decrementGrade();
+        std::cout << duck;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+}    
+
+    std::cout << "//////////////////////////////////////////" << std::endl;
+{
+    std::cout << RED "Invalid increment grade" RESET << std::endl;
+    try
+    {
+        Bureaucrat tone(std::string("Maria"), 1);
+        std::cout << tone;
+        
+        std::cout << "incrementGrade:\n";
+        tone.incrementGrade();
+        std::cout << tone;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+}
+    
+    std::cout << "//////////////////////////////////////////" << std::endl;
+{
+    std::cout << RED "Invalid decrement grade" RESET << std::endl;
+    try
+    {
+        Bureaucrat tone(std::string("Pikachu"), 150);
+        std::cout << tone;
+        
+        std::cout << "decrementGrade:\n";
+        tone.decrementGrade();
+        std::cout << tone;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+}    
+    return 0;
+}

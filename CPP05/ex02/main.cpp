d542e4e7ecbceb7e71e862efd8a1324f3c58c684
Artur Carvalho <arturcarvalho0507@gmail.com>
2025-02-14 22:02:46 +0000

    Added tests for Shrubbery form only

--- CPP05/ex02/main.cpp
+++ CPP05/ex02/main.cpp
@@ -6,15 +6,14 @@
 /*   By: fang <fang@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 20:25:49 by fang              #+#    #+#             */
-/*   Updated: 2025/02/13 20:41:03 by fang             ###   ########.fr       */
+/*   Updated: 2025/02/14 21:55:37 by fang             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "AForm.hpp"
 #include "Bureaucrat.hpp"
+#include "ShrubberyCreationForm.hpp"
 #include <iostream>
 
-#define RESET   "\033[0m"
 #define RED     "\033[31m"
 #define GREEN   "\033[32m"
 #define YELLOW  "\033[33m"
@@ -22,9 +21,79 @@
 #define MAGENTA "\033[35m"
 #define CYAN    "\033[36m"
 #define WHITE   "\033[37m"
+#define RESET   "\033[0m"
+#define CLEAR_SCREEN "\033[2J\033[1;1H"
+
 
 int main()
 { 
+    char ch;
+    std::cout << CLEAR_SCREEN;
+    std::cout << BLUE "//////////////////////////////////////////" RESET << std::endl;
+    std::cout << GREEN "Valid ShrubberyCreationForm execution" RESET << std::endl;
+    std::cout << GREEN "Burucrat with grade 10 (S:145 & E:137)" RESET << std::endl;
 
+    std::cout << std::endl;
+{
+    Bureaucrat b("Duck n1", 10);
+    ShrubberyCreationForm SCForm("Duck's Target");
+    std::cout << SCForm;
+    std::cout << std::endl;
+    
+    try {
+        SCForm.execute(b);
+    }catch(const std::exception& e){
+        std::cout << "Error: " << e.what() << std::endl;
+    }
+    std::cout << std::endl;
+    std::cout << SCForm;
+}
+std::cout << BLUE "//////////////////////////////////////////" RESET << std::endl;
+std::cin >> ch;
+std::cout << CLEAR_SCREEN;
+std::cout << BLUE "//////////////////////////////////////////" RESET << std::endl;
+    std::cout << RED "Invalid ShrubberyCreationForm execution" RESET << std::endl;
+    std::cout << RED "Burucrat with grade 150 (S:145 & E:137)" RESET << std::endl;
+    std::cout << std::endl;
+
+{
+    Bureaucrat b("Duck n2", 150);
+    ShrubberyCreationForm SCForm("Duck2's Target");
+    std::cout << SCForm;
+    std::cout << std::endl;
+
+    
+    try {
+        SCForm.execute(b);
+    }catch(const std::exception& e){
+        std::cout << "Error: " << e.what() << std::endl;
+    }
+    std::cout << std::endl;
+    std::cout << SCForm;
+}
+std::cout << BLUE "//////////////////////////////////////////" RESET << std::endl;
+std::cin >> ch;
+std::cout << CLEAR_SCREEN;
+std::cout << BLUE "//////////////////////////////////////////" RESET << std::endl;
+    std::cout << RED "Invalid ShrubberyCreationForm execution" RESET << std::endl;
+    std::cout << RED "Burucrat with grade 140 (S:145 & E:137)" RESET << std::endl;
+    std::cout << std::endl;
+
+{
+    Bureaucrat b("Duck n3", 140);
+    ShrubberyCreationForm SCForm("Duck3's Target");
+    std::cout << SCForm;
+    std::cout << std::endl;
+
+    
+    try {
+        SCForm.execute(b);
+    }catch(const std::exception& e){
+        std::cout << "Error: " << e.what() << std::endl;
+    }
+    std::cout << std::endl;
+    std::cout << SCForm;
+}
+std::cout << BLUE "//////////////////////////////////////////" RESET << std::endl;
     return 0;
 }

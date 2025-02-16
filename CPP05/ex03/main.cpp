7e632f687c78ccd02e6a9f88b5aa796428fdfbb4
Artur Carvalho <arturcarvalho0507@gmail.com>
2025-02-16 16:36:18 +0000

    CPP05::ex03 Added tests for intern.

--- CPP05/ex03/main.cpp
+++ CPP05/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fang <fang@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 20:25:49 by fang              #+#    #+#             */
-/*   Updated: 2025/02/16 14:06:46 by fang             ###   ########.fr       */
+/*   Updated: 2025/02/16 16:33:25 by fang             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,94 @@
 #include "ShrubberyCreationForm.hpp"
 #include "RobotomyRequestForm.hpp"
 #include "PresidentialPardonForm.hpp"
+#include "colors.hpp"
 #include <iostream>
+#include "Intern.hpp"
 
-#define RED     "\033[31m"
-#define GREEN   "\033[32m"
-#define YELLOW  "\033[33m"
-#define BLUE    "\033[34m"
-#define MAGENTA "\033[35m"
-#define CYAN    "\033[36m"
-#define WHITE   "\033[37m"
-#define RESET   "\033[0m"
-#define CLEAR_SCREEN "\033[2J\033[1;1H"
-
-int main() 
+int main()
 {
-    
+    Intern josh;
+    Bureaucrat Toni("Toni", 10);
+    AForm *f1, *f2, *f3;
+
+    std::cout << YELLOW "Toni asks josh to create forms: " RESET << std::endl;
+    try
+    {
+        std::cout << std::endl;
+        f1 = josh.makeForm("shrubberry creation", "Farm");
+        std::cout << *f1;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    std::cout << std::endl;
+
+    try
+    {
+        std::cout << std::endl;
+        f2 = josh.makeForm("robotomy", "Artur");
+        std::cout << *f2;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    try
+    {
+        std::cout << std::endl;
+        f3 = josh.makeForm("presidential pardon", "Toni");
+        std::cout << *f3;
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    try
+    {
+        std::cout << std::endl;
+        Toni.signForm(*f2);
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    try
+    {
+        std::cout << std::endl;
+        Toni.executeForm(*f2);
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    try
+    {
+        std::cout << std::endl;
+        Toni.signForm(*f3);
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    try
+    {
+        Toni.executeForm(*f3);
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+        std::cout << "Toni: oops" << std::endl;
+    }
+
+    delete f1;
+    delete f2;
+    delete f3;
     return 0;
 }

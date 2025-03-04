3fd813a3f5e109fd70b252627db3195a826d23fd
Artur Carvalho <arturcarvalho0507@gmail.com>
2025-03-04 17:24:18 +0000

    Removed todo's

--- CPP06/ex00/main.cpp
+++ CPP06/ex00/main.cpp
@@ -3,18 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fang <fang@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: artuda-s <artuda-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 11:57:33 by artuda-s          #+#    #+#             */
-/*   Updated: 2025/03/03 23:08:41 by fang             ###   ########.fr       */
+/*   Updated: 2025/03/04 13:58:48 by artuda-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
-
-//todo nan e infs nao estao a sair direito
-//todo testar e go on
-
 
 int main(int ac, char *av[])
 {

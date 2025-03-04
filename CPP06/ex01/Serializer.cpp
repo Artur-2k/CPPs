2a73421d8c84343f7685f5ef62277d26a6072cd6
Artur Carvalho <arturcarvalho0507@gmail.com>
2025-03-04 17:23:32 +0000

    CPP06::ex01 Added serializing class as asked in the subject

---
+++ CPP06/ex01/Serializer.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Serializer.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: artuda-s <artuda-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/04 17:00:34 by artuda-s          #+#    #+#             */
+/*   Updated: 2025/03/04 17:01:09 by artuda-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Serializer.hpp"
+
+Serializer::Serializer() {}
+Serializer::~Serializer() {}
+Serializer::Serializer(const Serializer &other) {(void)other;}
+Serializer& Serializer::operator=(const Serializer &other) {(void)other; return *this;}
+
+uintptr_t Serializer::serialize(Data* ptr)
+{
+    return reinterpret_cast<uintptr_t>(ptr);
+}
+
+Data* Serializer::deserialize(uintptr_t raw)
+{
+    return reinterpret_cast<Data *>(raw);
+}

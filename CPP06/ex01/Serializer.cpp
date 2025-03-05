309cc40fc81f015b13e80f887efc33f8265f8ff0
Artur Carvalho <arturcarvalho0507@gmail.com>
2025-03-05 12:17:35 +0000

    CPP06::ex01 nullptr -> NULL

--- CPP06/ex01/Serializer.cpp
+++ CPP06/ex01/Serializer.cpp
@@ -6,7 +6,7 @@
 /*   By: artuda-s <artuda-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 17:00:34 by artuda-s          #+#    #+#             */
-/*   Updated: 2025/03/04 17:24:57 by artuda-s         ###   ########.fr       */
+/*   Updated: 2025/03/05 12:17:00 by artuda-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,5 @@ uintptr_t Serializer::serialize(Data* ptr)
 
 Data* Serializer::deserialize(uintptr_t raw)
 {
-    return raw ? reinterpret_cast<Data *>(raw) : nullptr;
+    return raw ? reinterpret_cast<Data *>(raw) : NULL;
 }

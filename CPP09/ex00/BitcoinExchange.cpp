a8b80572870283a08fa7a04741f3f24d13eb2474
Artur Carvalho <arturcarvalho0507@gmail.com>
2025-04-07 17:59:56 +0100

    CPP09::ex00 added unfinished stuff. will work later

---
+++ CPP09/ex00/BitcoinExchange.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: artuda-s <artuda-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/07 16:44:10 by artuda-s          #+#    #+#             */
+/*   Updated: 2025/04/07 16:45:00 by artuda-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "BitcoinExchange.hpp"
+
+BitcoinExchange::BitcoinExchange(/* args */)
+{
+}
+
+BitcoinExchange::~BitcoinExchange()
+{
+}
---
+++ CPP09/ex00/main.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: artuda-s <artuda-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/07 16:43:25 by artuda-s          #+#    #+#             */
+/*   Updated: 2025/04/07 16:48:17 by artuda-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// btc input.txt
+// yyyy-mm-dd | value
+// 2009-01-02,0
+// 2045 -> will use the lower bound date
+// value must be between 0 and 1000 inclusive
+
+int main(int argc, char const *argv[])
+{
+    
+
+    return 0;
+}

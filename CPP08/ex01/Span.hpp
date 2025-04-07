5a104cde01c5a0b23ae2f9fc3a33cf867198abdb
Artur Carvalho <arturcarvalho0507@gmail.com>
2025-04-07 12:06:42 +0100

    CPP08::ex01 Added hpp file and makefile

---
+++ CPP08/ex01/Span.hpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Span.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: artuda-s <artuda-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/02 15:30:25 by artuda-s          #+#    #+#             */
+/*   Updated: 2025/04/07 11:41:06 by artuda-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <stdexcept>
+#include <vector>
+#include <algorithm>
+#include <iostream> //todo delete
+
+class Span
+{
+private:
+    unsigned int _N;
+    public:
+    std::vector<int> _vec;
+    Span();
+    Span(unsigned int N);
+    Span(const Span& other);
+    ~Span();
+    
+    Span& operator= (const Span& other);
+    
+    unsigned int getMaxCapacity() const;
+    
+    // adds a new number if not at max number capacity (_N). if that happens throws an exception
+    void addNumber(int num); 
+    
+    int shortestSpan() const;
+    int longestSpan() const;
+};

45ad344f1bcaa3803a3927240a33e615f8ae9134
Artur Carvalho <arturcarvalho0507@gmail.com>
2025-08-18 10:53:16 +0100

    CPP07::ex02 Updated main tests for out of bounds accessing []

--- CPP07/ex02/main.cpp
+++ CPP07/ex02/main.cpp
@@ -15,7 +15,8 @@
 int main()
 {
     Array<char> teste;
-    
+    std::cout << "size of empty array should be 0: " << teste.size() << "\n" << std::endl;
+
     Array<char> teste2(10);
     teste2[0] = 'O';
     teste2[1] = 'l';
@@ -24,6 +25,12 @@ int main()
 
     Array<char> teste3(teste2);
     
+
+    Array<const int> teste4(10);
+    // teste4[0] = 'X'; // this causes error but we can access it still
+    std::cout << "size of const array should be 10: " << teste4.size() << "\n" << std::endl;
+    
+
     std::cout << "size of teste2 should be 10 (3 populated 7 0'ed): " << teste2.size() << std::endl;
     
     std::cout << "Teste2: " << teste2[0] << teste2[1] << teste2[2] << std::endl;
@@ -34,5 +41,18 @@ int main()
 
     std::cout << "Teste2: " << teste2[0] << teste2[1] << teste2[2] << std::endl;
     std::cout << "Teste3: " << teste3[0] << teste3[1] << teste3[2] << std::endl;
+
+    try
+    {
+        std::cout << "Trying to access out of bounds: " << std::endl;
+        std::cout << teste2[69] << std::endl; // This should throw an exception
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
+
+    
     return 0;
 }
